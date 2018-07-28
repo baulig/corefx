c566abb961970495bcd4f4b484566b1d3f07b7dc
Martin Baulig <mabaul@microsoft.com>
2018-07-28 03:48:03 -0400

    Split pal_rsa.c

---
+++ src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_rsa_ios.h
@@ -0,0 +1,18 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#pragma once
+
+#include "pal_rsa.h"
+
+/*
+Generate a new RSA keypair with the specified key size, in bits.
+
+Returns 1 on success, 0 on failure.  On failure, *pOSStatus should contain the OS reported error.
+*/
+DLLEXPORT int32_t AppleCryptoNative_RsaGenerateKey(int32_t keySizeBits,
+                                                   SecKeyRef* pPublicKey,
+                                                   SecKeyRef* pPrivateKey,
+                                                   int32_t* pOSStatus);
+
