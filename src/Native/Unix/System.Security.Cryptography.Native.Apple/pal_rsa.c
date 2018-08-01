73e005bda301c0e7ac4724147e7f52f678056519
Martin Baulig <mabaul@microsoft.com>
2018-07-31 21:39:25 -0400

    X

--- src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_rsa.c
+++ src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_rsa.c
@@ -4,7 +4,11 @@
 
 #include "pal_rsa.h"
 
-#if REQUIRE_MAC_SDK_VERSION(10_12) || REQUIRE_IOS_SDK_VERSION(11_4)
+#if REQUIRE_MAC_SDK_VERSION(10_12)
+
+//
+// These APIs are also available on iOS 10
+//
 
 static int32_t RsaPrimitive(SecKeyRef key,
                             uint8_t* pbData,
